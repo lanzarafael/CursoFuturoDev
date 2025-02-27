45b86fbf422dd6fe8d08264847009166045fe756
rafael <rafaelmutu10@gmail.com>
2025-02-26 21:14:03 -0300

    novo codigo

---
+++ Aula 1/src/aula1/Aula1.java
@@ -0,0 +1,15 @@
+package aula1;
+
+public class Aula1 {
+    public static void main(String[] args){
+        System.out.println("Olá, Mundo!");
+        System.out.println("Segunda linha");
+
+        int anoNascimento = 1984;
+
+        System.out.println(anoNascimento);
+
+        int idade = 2025 - anoNascimento;
+        System.out.println("Idade: " + idade);
+    }
+}
